7289cfc68b72f3ef4dee8436275e452bbeb49c41
arongyenes-srg <aron.gyenes@srg.hu>
2019-05-10 17:52:59 +0200

    observers, subjects

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+import 'rxjs/Rx';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  numbersObsSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    const myNumbers = Observable.interval(1000)
+      .map(
+        (data: number) => {
+          return data * 2;
+        }
+      );
+    myNumbers.subscribe(
+      (number: number) => {
+        console.log(number);
+      }
+    );
+
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      }, 2000);
+      setTimeout(() => {
+        observer.next('second package');
+      }, 4000);
+      // setTimeout(() => {
+      //   observer.error('this does not work');
+      // }, 5000);
+      setTimeout(() => {
+        observer.complete();
+      }, 5000);      
+    });
+    this.numbersObsSubscription = myObservable.subscribe(
+      (data: string) => { console.log(data) },
+      (error: string) => { console.log(error) },
+      () => { console.log('completed'); }
+    );
+  }
+
+  ngOnDestroy() {
+    this.numbersObsSubscription.unsubscribe();
   }
 
 }
